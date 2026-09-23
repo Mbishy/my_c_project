69cb1f9589f3e8ef3721e5c55e8f0bd0ed72fe54
Mbishy <102670808+Mbishy@users.noreply.github.com>
2026-09-23 15:59:58 +0500

    add funtion print

--- main.c
+++ main.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
-int main() {
+
+void print_f(char *name);
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>/n", argv[0]);
+        return 1;
+    }
+
+    print_f(argv[1]);
     return 0;
+}
+
+void print_f(char *name) {
+    FILE *f = fopen(name, "rt");
+
+    if (f != NULL) {
+        int c = fgetc(f);
+        while (c != EOF) {
+            putc(c, stdout);
+            c = fgetc(f);
+        }
+        fclose(f);
+    }
 }
